366d6a898181536a976eb180a2d826a44b2368fa
ofekabo-Laptop <76842189+ofekabo@users.noreply.github.com>
2021-07-14 11:43:29 +0300

    bit of lightning

---
+++ Assets/EmissiveControl.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class EmissiveControl : MonoBehaviour
+{
+    [SerializeField] private Material mat;
+    [Range(0.0f,0.3f)]
+    [SerializeField] private float value;
+
+    private void Update()
+    {
+        mat.SetColor("_EmissionColor",new Color(255,255,255,0.5f) * value);
+    }
+}
