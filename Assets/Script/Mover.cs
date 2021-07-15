01b2c6163d50d6c8d8c16ec0beca53dae48ced65
Tomer <63203688+tomerc133@users.noreply.github.com>
2021-07-15 14:15:28 +0300

    lvl update

--- Assets/Script/Mover.cs
+++ Assets/Script/Mover.cs
@@ -10,7 +10,7 @@ public class Mover : MonoBehaviour
 {
     [SerializeField] float speed;
     [SerializeField] private ParticleSystem _pps;
-    
+
     private Rigidbody _rb;
     [HideInInspector] public bool gotHit;
     private Outline _outline;
@@ -22,8 +22,8 @@ public class Mover : MonoBehaviour
         _rb.useGravity = false;
         gotHit = false;
         _outline = GetComponent<Outline>();
-
     }
+
     private void Start()
     {
         _rb = GetComponent<Rigidbody>();
@@ -33,32 +33,37 @@ public class Mover : MonoBehaviour
     }
 
     private void Update()
-    {
-        transform.Translate(Vector3.down * speed * Time.deltaTime,Space.World);
-    }
 
-    // void FixedUpdate()
-    // {
-    //
-    //     if (gotHit)
-    //     {
-    //         _rb.useGravity = true;
-    //         // float maxVelY = Mathf.Clamp(_rb.velocity.y,-15,2);
-    //         // _rb.velocity = new Vector3(_rb.velocity.x,maxVelY,_rb.velocity.z);
-    //         return;
-    //     }
-    //     _rb.MovePosition(transform.position + Vector3.down * Time.deltaTime * speed);
-    //     
-    // }
+    {
+        if (!gotHit)
+        {
+            transform.Translate(Vector3.down * speed * Time.deltaTime, Space.World);
+            return;
+        }
+
+        // void FixedUpdate()
+        // {
+        //
+        //     if (gotHit)
+        //     {
+        //         _rb.useGravity = true;
+        //         // float maxVelY = Mathf.Clamp(_rb.velocity.y,-15,2);
+        //         // _rb.velocity = new Vector3(_rb.velocity.x,maxVelY,_rb.velocity.z);
+        //         return;
+        //     }
+        //     _rb.MovePosition(transform.position + Vector3.down * Time.deltaTime * speed);
+        //     
+        // }
+        _rb.useGravity = true;
+    }
 
     private void OnCollisionEnter(Collision other)
-    {
-        // if (other.collider.CompareTag("Obstacle"))
-        // {
+        {
+            // if (other.collider.CompareTag("Obstacle"))
+            // {
             gotHit = true;
-            Instantiate(_pps,transform.position,_pps.transform.rotation);
+            Instantiate(_pps, transform.position, _pps.transform.rotation);
             _outline.enabled = false;
             // }
-
+        }
     }
-}
