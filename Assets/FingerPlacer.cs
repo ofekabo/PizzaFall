3d6fb2f385f7a683ba61c05567a97470ff9d6928
ofekabo <76842189+ofekabo@users.noreply.github.com>
2021-07-18 16:21:05 +0300

    push fingers

---
+++ Assets/FingerPlacer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FingerPlacer : MonoBehaviour
+{
+    [SerializeField] Transform finger;
+    [SerializeField] GameObject clickIndicator;
+    [SerializeField]  Vector2 fingerOffset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        clickIndicator.SetActive(false);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        finger.position = GetScreenPos();
+        if (Input.GetMouseButtonDown(0))
+        {
+            StartCoroutine(FlashIndicator());
+        }
+        
+    }
+
+    Vector2 GetScreenPos()
+    {
+        Vector3 posn = Input.mousePosition;
+        return new Vector2(posn.x,posn.y) + fingerOffset;
+    }
+
+    IEnumerator FlashIndicator()
+    {
+        clickIndicator.SetActive(true);
+        yield return new WaitForSeconds(0.1f);
+        clickIndicator.SetActive(false);
+    }
+}
