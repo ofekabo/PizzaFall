50c47657b4e1101e692492bade576bc36037a111
Tomer <63203688+tomerc133@users.noreply.github.com>
2021-07-18 12:57:10 +0300

    ninja hand

--- Assets/TriggerPps.cs
+++ Assets/TriggerPps.cs
@@ -3,13 +3,17 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
 
 public class TriggerPps : MonoBehaviour
 {
     [SerializeField] private ParticleSystem pps;
+    [SerializeField] private PlayableDirector _director;
 
     private void OnTriggerEnter(Collider other)
     {
         Instantiate(pps, transform.position, pps.transform.rotation);
+        _director.Play();
     }
 }
