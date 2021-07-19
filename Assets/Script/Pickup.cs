0b54c3055b50ca8bba39e076c46019c2dce441c3
tomerc133 <63203688+tomerc133@users.noreply.github.com>
2021-07-19 12:00:15 +0300

    fbxsx

--- Assets/Script/Pickup.cs
+++ Assets/Script/Pickup.cs
@@ -9,14 +9,24 @@ public class Pickup : MonoBehaviour
     [SerializeField] private Texture _texture;
 
     [SerializeField] private ParticleSystem ppSystem;
+
+    [SerializeField] private GameObject x2Object;
     
     // [SerializeField] private Material pizzaMaterial;
+
+    private void Start()
+    {
+     
+           
+        
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         // pizzaMaterial.SetTexture("_MainTex",_texture);
         other.GetComponent<Renderer>().material.mainTexture = _texture;
         Instantiate(ppSystem, transform.position,quaternion.identity);
+        Instantiate(x2Object, transform.position + (Vector3.up * 3), Quaternion.identity);
         Destroy(gameObject,0.1f);
     }
 }
