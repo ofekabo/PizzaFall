7b42feb37b8e7ae2fd78be43232cf4bdba1b1b53
ofekabo-Laptop <76842189+ofekabo@users.noreply.github.com>
2021-07-19 11:55:14 +0300

    moveincircle

---
+++ Assets/Script/PizzaRotator.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class PizzaRotator : MonoBehaviour
+{
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        // Vector3 dir = Camera.main.WorldToScreenPoint(GetMousePos());
+        // Debug.Log(GetMousePos());
+        // float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg - 90f;
+        // transform.rotation = Quaternion.Euler(90, angle, 0);
+        Vector3 dir = GetMousePos() - Camera.main.WorldToScreenPoint(transform.position);
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg - 90f;
+
+        Quaternion targetRotation = Quaternion.AngleAxis(angle, Vector3.down);
+
+        // Quaternion targetRotation = Quaternion.Euler(90,angle,0);
+        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation , Time.deltaTime * 3);
+        // transform.rotation = Quaternion.Euler(90, transform.rotation.y, transform.rotation.z);
+    }
+
+    Vector3 GetMousePos()
+    {
+        return new Vector3(Input.mousePosition.x, Input.mousePosition.y,transform.position.z);
+    }
+}
+
