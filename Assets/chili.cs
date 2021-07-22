7f15cf1b739a6a49b277101c38c0785e2c3a52fc
Tomer <63203688+tomerc133@users.noreply.github.com>
2021-07-22 19:34:34 +0300

    post prosses update

--- Assets/chili.cs
+++ Assets/chili.cs
@@ -3,13 +3,27 @@ using System.Collections;
 using System.Collections.Generic;
 using EZCameraShake;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class chili : MonoBehaviour
 {
+    [SerializeField] private GameObject fire;
+    [SerializeField] private VolumeProfile fireVolume;
+    [SerializeField] private VolumeProfile normalVolume;
+    [SerializeField] private Volume _volumeProfile;
+    
     private void OnTriggerEnter(Collider other)
     {
-       
         CameraShaker.Instance.ShakeOnce(2, 2, 0.1f, 0.1f);
-        Destroy(gameObject, 0.2f);
+        StartCoroutine(FireEffect());
+    }
+
+    IEnumerator FireEffect()
+    {
+        fire.SetActive(true);
+        _volumeProfile.profile = fireVolume;
+        yield return new WaitForSeconds(5f);
+        fire.SetActive(false);
+        _volumeProfile.profile = normalVolume;
     }
 }
